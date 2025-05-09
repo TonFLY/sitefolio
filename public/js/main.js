1e1a2635125b399692624cce611277b80f7c8024
Wellington Marques da Silva Miranda <techwellington.dev@gmail.com>
2025-05-09 15:51:25 -0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+// Exemplo: Scroll suave para âncoras internas
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
